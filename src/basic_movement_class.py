496bb68bdd1c7924d571eda20e51ad8ad237cccd
jimzers <adamleesaintfrancis@gmail.com>
2020-08-23 16:48:43 -0700

    add drone read and write movement

--- src/basic_movement_class.py
+++ src/basic_movement_class.py
@@ -1,7 +1,7 @@
 import airsim
 import numpy as np
 import cv2
-from random import randint
+from random import randint, choice
 
 client = airsim.MultirotorClient()
 client.confirmConnection()
@@ -12,19 +12,30 @@ client.takeoffAsync().join()
 
 direction_mod_offset = 0
 
-direction_arr = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0)]
+direction_arr = [(1, 0, 0), (0, 1, 0), (-1, 0, 0), (0, -1, 0)]
 
 
 class Drone:
-    def __init__(self, logging=False):
+    def __init__(self, logging=True):
         self.velocity = 5
         self.direction_mod_offset = 0
         self.direction_arr = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0)]
+        self.input_file_name = ("./src/input_commands.txt")
+        self.output_file_name = ("./src/output_commands.txt")
         self.quad_offset_mapping = {
             'forward': direction_arr[self.direction_mod_offset % len(self.direction_arr)],
-            'backward': direction_arr[(self.direction_mod_offset + 1) % len(self.direction_arr)],
-            'right': direction_arr[(self.direction_mod_offset + 2) % len(self.direction_arr)],
+            'right': direction_arr[(self.direction_mod_offset + 1) % len(self.direction_arr)],
+            'backward': direction_arr[(self.direction_mod_offset + 2) % len(self.direction_arr)],
             'left': direction_arr[(self.direction_mod_offset + 3) % len(self.direction_arr)],
+        }
+
+        self.movements = {
+            'forward': self.move_forward,
+            'backward': self.move_backward,
+            'right': self.move_right,
+            'left': self.move_left,
+            'up': self.move_up,
+            'down': self.move_down
         }
 
         self.logging = logging
@@ -33,8 +44,8 @@ class Drone:
     def calculate_offset_mapping(self):
         self.quad_offset_mapping = {
             'forward': direction_arr[self.direction_mod_offset % len(self.direction_arr)],
-            'backward': direction_arr[(self.direction_mod_offset + 1) % len(self.direction_arr)],
-            'right': direction_arr[(self.direction_mod_offset + 2) % len(self.direction_arr)],
+            'right': direction_arr[(self.direction_mod_offset + 1) % len(self.direction_arr)],
+            'backward': direction_arr[(self.direction_mod_offset + 2) % len(self.direction_arr)],
             'left': direction_arr[(self.direction_mod_offset + 3) % len(self.direction_arr)],
         }
 
@@ -84,25 +95,82 @@ class Drone:
 
     def write_log(self):
         print('Writing logs...')
-        with open('movement_log.txt') as f:
-            res = '\n'.join(self.log_arr)
-            f.write(res)
+        f = open('./src/movement_log.txt', "w")
+        for command in self.log_arr:
+            f.write(command + "\n")
         print('Writing finished')
 
+    def pick_movement(self, movement, rand_chance=0.1):
+        rand_num = np.random.rand()
+        move = self.movements[movement]
+        move_str = movement
+        if rand_num > 1.0 - rand_chance:
+            # do random action now
+            print("----MOVEMENT HAS CHANGED----")
+            move_str = choice(list(self.movements.keys()))
+            move = self.movements[move_str]
+        # return the movement
+        return move, move_str
 
+    def record_pose(self):
+        print(client.getMultirotorState().kinematics_estimated)
+        print(client.getMultirotorState().kinematics_estimated.orientation)
+
+
+
+    def get_np_image(self, save_image=False, filename="curr_image.png"):
+        responses = client.simGetImages([airsim.ImageRequest("front_left", airsim.ImageType.Scene, False, False)])
+        response = responses[0]
+
+        # get numpy array
+        img1d = np.fromstring(response.image_data_uint8, dtype=np.uint8)
+
+        # reshape array to 4 channel image array H X W X 4
+        img_rgb = img1d.reshape(response.height, response.width, 3)
+
+        # # original image is fliped vertically
+        # img_rgb = np.flipud(img_rgb)
+
+        if save_image:
+            cv2.imwrite(filename, img_rgb)
+
+        return img_rgb
+
+    def move(self):
+
+        input = open(self.input_file_name, "r")
+        for command in input:
+            cleaned_cmd = command.strip()
+            if cleaned_cmd in self.movements:
+                movement, move_str = self.pick_movement(cleaned_cmd)
+                for i in range(0,2):
+                    movement()
+                self.record_pose()
+                self.get_np_image(True, "curr_image.png")
+                airsim.time.sleep(1)
+        input.close()
+        self.write_log()
+        print("finished")
 d = Drone()
-while True:
-    d.move_forward()
-    value = randint(0, 3)
-    if value == 0:
-        for i in range(0, 3):
-            d.turn_left()
-    elif value == 1:
-        for i in range(0, 3):
-            d.turn_right()
-    elif value == 2:
-        for i in range(0, 3):
-            d.move_down()
-    else:
-        for i in range(0, 3):
-            d.move_up()
+d.move()
+# d.turn_right()
+# d.move_forward()
+# airsim.time.sleep(1)
+# d.turn_left()
+# d.move_forward()
+# airsim.time.sleep(1)
+# d.turn_left()
+# d.move_forward()
+# airsim.time.sleep(1)
+# d.turn_left()
+# d.move_forward()
+# airsim.time.sleep(1)
+# d.turn_left()
+# d.move_forward()
+# airsim.time.sleep(1)
+
+# exit protocol
+client.armDisarm(False)
+client.reset()
+
+client.enableApiControl(False)
