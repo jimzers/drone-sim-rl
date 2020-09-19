8cd5055fcabf24147b2d170b021ff74120fb3fd9
akshaygopalkr <agopalkr@ucsd.edu>
2020-09-19 10:43:35 -0700

    Documentation for basic_movement_class.py

--- src/basic_movement_class.py
+++ src/basic_movement_class.py
@@ -11,7 +11,12 @@ direction_arr = [(1, 0, 0), (0, 1, 0), (-1, 0, 0), (0, -1, 0)]
 
 
 class Drone:
+    """ Contains basic movement control for an Airsim drone """
     def __init__(self, logging=True):
+        """
+        Initializes the drone object with a specific velocity and the
+        quad offset map to keep track of where the drone is facing
+        """
         self.velocity = 4
         self.direction_mod_offset = 0
         self.direction_arr = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0)]
@@ -37,6 +42,10 @@ class Drone:
         self.log_arr = []
 
     def calculate_offset_mapping(self):
+        """
+        Recalculates the quad offset map to keep
+        track of the direction the drone is facing
+        """
         self.quad_offset_mapping = {
             'forward': direction_arr[self.direction_mod_offset % len(self.direction_arr)],
             'right': direction_arr[(self.direction_mod_offset + 1) % len(self.direction_arr)],
@@ -45,38 +54,59 @@ class Drone:
         }
 
     def turn_right(self):
+        """ Turns the drone right  """
         self.direction_mod_offset += 1
         self.calculate_offset_mapping()
         direction_num = self.direction_mod_offset % len(self.direction_arr)
         client.rotateToYawAsync(direction_num * 90).join()
 
     def turn_left(self):
+        """ Turns the drone left  """
         self.direction_mod_offset -= 1
         self.calculate_offset_mapping()
         direction_num = self.direction_mod_offset % len(self.direction_arr)
         client.rotateToYawAsync(direction_num * 90).join()
 
     def move_left(self,distance):
+        """
+        Turns the drone left and then moves forward
+        :param distance: The distance the drone will move left
+        """
         self.turn_left()
         self.move_forward(distance)
         # self.log_arr.append("left")
 
     def move_right(self,distance):
+        """
+        Turns the drone right and then moves forward
+        :param distance: The distance the drone will move right
+        """
         self.turn_right()
         self.move_forward(distance)
         # self.log_arr.append("right")
 
     def move_up(self,distance):
+        """
+        Moves the drone up for a certain distance
+        :param distance: the distance the drone moves up
+        """
         client.moveByVelocityAsync(0, 0, 1, 0.3).join()
         # if self.logging:
         #     self.log_arr.append("up")
 
     def move_down(self):
+        """
+        Moves the drone down for a certain distance
+        """
         client.moveByVelocityAsync(0, 0, -1, 0.3).join()
         # if self.logging:
         #     self.log_arr.append("down")
 
     def move_forward(self, distance):
+        """
+        Moves the drone forward for a certain distance
+        :param distance: The distance the drone will move forward
+        """
         quad_offset = self.quad_offset_mapping['forward']
         client.moveByVelocityAsync(self.velocity * quad_offset[0], self.velocity * quad_offset[1],
                                    0.15, distance/self.velocity).join()
@@ -84,6 +114,10 @@ class Drone:
         #     self.log_arr.append("forward")
 
     def move_backward(self, distance):
+        """
+        Moves the drone backward for a certain distance
+        :param distance: The distance the drone will move backward
+        """
         quad_offset = self.quad_offset_mapping['backward']
         client.moveByVelocityAsync(self.velocity * quad_offset[0], self.velocity * quad_offset[1],
                                    0.15, distance/self.velocity).join()
@@ -91,6 +125,10 @@ class Drone:
         #     self.log_arr.append("backward")
 
     def write_log(self, logfile='./src/movement_log.txt'):
+        """
+        Writes a log describing all the movements the drone performed
+        :param logfile: The file the log will be written to
+        """
         # TODO: parameterize logfile name
         print('Writing logs...')
         f = open(logfile, "w")
@@ -99,6 +137,13 @@ class Drone:
         print('Writing finished')
 
     def pick_movement(self, movement, rand_chance=0.1):
+        """
+        Has a probability to change the initial movement of the drone
+        by 10%, otherwise continues to move in the same direction
+        :param movement: the initial movement of the drone
+        :param rand_chance: The chance the drone will change its movement
+        :return: the new movement and the movement represented by a string
+        """
         rand_num = np.random.rand()
         move = self.movements[movement]
         move_str = movement
@@ -111,6 +156,10 @@ class Drone:
         return move, move_str
 
     def record_pose(self):
+        """
+        Records the pose of the drone and adds the pose to the logging array
+        :return: the pose in a formatted string
+        """
         drone_pos = client.getMultirotorState().kinematics_estimated.position
         output = " | ".join(["x_val: " + str(drone_pos.x_val), "y_val: " + str(drone_pos.y_val), "z_val: " + str(drone_pos.z_val)])
         print(output)
@@ -120,6 +169,12 @@ class Drone:
 
 
     def get_np_image(self, save_image=False, filename="curr_image.png"):
+        """
+        takes an image from the drone's camera in Unreal Engine.
+        :param save_image: Whether or not to save the image
+        :param filename: The file the image will be saved in
+        :return: the numpy array representing the image
+        """
         responses = client.simGetImages([airsim.ImageRequest("front_left", airsim.ImageType.Scene, False, False)])
         response = responses[0]
 
@@ -138,7 +193,13 @@ class Drone:
         return img_rgb
 
     def move(self, output_file, input_file, no_input=False):
-
+        """
+        Moves the drone using a file with movement commands. Adds in an additional
+        10% noise that can potentially change the drone's movement.
+        :param output_file: the file the logging file will write too
+        :param input_file: the file with all the movements the drone will perform
+        :param no_input: whether or not the drone should read from an input file
+        """
         input = open(input_file, "r")
         for command in input:
             cleaned_cmd = command.strip()
@@ -158,9 +219,15 @@ class Drone:
         print("finished episode")
 
     def clear_logging_arr(self):
+        """ Clears the logging array """
         self.log_arr = []
 
     def run_multiple(self, num_episodes=5, base_output_name="logs/output_command"):
+        """
+        Runs the movement method for multiple episodes
+        :param num_episodes: the number of episodes to be performed
+        :param base_output_name: the base file name for all the movements of the drone
+        """
         for i in range(num_episodes):
             client.reset()
             client.confirmConnection()
